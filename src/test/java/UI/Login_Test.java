430278c7277bd59cc8ffc908e0c70b1743b20848
Dasun Sooriyabandara <115138972+DasunSooriyabandara@users.noreply.github.com>
2024-02-26 23:48:16 +0530

    Login to the site
    This code is to test the login

---
+++ src/test/java/UI/Login_Test.java
@@ -0,0 +1,38 @@
+package UI;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Login_Test {
+
+	 public static void main(String[] args) {
+	        // Set up ChromeDriver using WebDriverManager
+	        WebDriverManager.chromedriver().setup();
+	        
+	        // Initialize ChromeDriver
+	        ChromeDriver driver = new ChromeDriver();
+	        
+	        // Open Sauce Demo website
+	        driver.get("https://www.saucedemo.com/");
+	        
+	        // Find the username field and enter username
+	        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+	        
+	        // Find the password field and enter password
+	        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+	        
+	        // Find and click the login button
+	        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+	        
+	        // Close the browser window
+	        //driver.quit();
+		
+
+	}
+
+}
+
+
+
+
